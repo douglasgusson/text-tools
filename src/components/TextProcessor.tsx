56afccd311940d2edc3413a2a57f6010adfd61f5
Douglas Gusson <douglasgusson@gmail.com>
2025-05-07 06:56:15 -0300

    Adiciona melhorias no layout

--- src/components/TextProcessor.tsx
+++ src/components/TextProcessor.tsx
@@ -31,14 +31,14 @@ export function TextProcessor() {
   return (
     <div className="flex flex-col w-screen h-screen">
       <div className="bg-gray-800 p-2 flex items-center">
-        <div className="flex space-x-2">
+        <div className="flex space-x-2 flex-1">
           <div className="w-3 h-3 rounded-full bg-red-500"></div>
           <div className="w-3 h-3 rounded-full bg-yellow-500"></div>
           <div className="w-3 h-3 rounded-full bg-green-500"></div>
         </div>
 
         <select
-          className="bg-gray-700 text-gray-300 text-sm mx-auto px-2 py-1 rounded border-0 focus:ring-1 focus:ring-blue-500 focus:outline-none"
+          className="flex-1 max-w-2xs bg-zinc-700 text-zinc-300 text-sm mx-auto px-2 py-1 rounded border-0 focus:ring-1 focus:ring-blue-500 focus:outline-none"
           value={workerType}
           onChange={(e) => setWorkerType(e.target.value as WorkerType)}
         >
@@ -49,10 +49,28 @@ export function TextProcessor() {
             {workersLabels[WorkerType.prettifyJson]}
           </option>
         </select>
+
+        <div className="flex flex-row-reverse gap-2 flex-1">
+          <button
+            onClick={handleProcess}
+            className="bg-blue-600 hover:bg-blue-500 font-semibold text-white px-3 py-1 rounded text-xs flex items-center"
+          >
+            Processar
+            <span className="ml-1 bg-black/20 rounded px-0.5 font-mono text-gray-300 text-xs">
+              <kbd>Ctrl</kbd>+<kbd>Enter</kbd>
+            </span>
+          </button>
+          <button
+            onClick={handleClear}
+            className="bg-orange-600 hover:bg-orange-500 font-semibold text-white px-3 py-1 rounded text-xs flex items-center"
+          >
+            Limpar
+          </button>
+        </div>
       </div>
       <div className="flex h-full">
         <textarea
-          className="flex-1 h-full bg-gray-900 text-gray-300 font-mono p-4 outline-none resize-y border-0 focus:ring-2 focus:ring-blue-500 text-sm leading-relaxed"
+          className="flex-1 h-full bg-zinc-800 text-zinc-200 font-mono p-4 outline-none resize-y border-0 focus:ring-2 focus:ring-blue-500 text-sm leading-relaxed"
           rows={10}
           value={input}
           onChange={(e) => setInput(e.target.value)}
@@ -65,27 +83,12 @@ export function TextProcessor() {
             }
           }}
         />
-        <pre className="flex-1 bg-gray-800 text-gray-200 font-mono p-4">
+        <pre className="flex-1 bg-stone-800 text-stone-200 font-mono p-4">
           {output}
         </pre>
       </div>
       <div className="flex justify-between items-center text-xs text-gray-400 bg-gray-800 p-2">
         <div>Caracteres: {input.length}</div>
-        <div className="flex flex-row-reverse gap-2">
-          <button
-            onClick={handleProcess}
-            className="bg-blue-600 hover:bg-blue-500 font-semibold text-white px-3 py-1 rounded text-xs flex items-center"
-          >
-            Processar{" "}
-            <span className="ml-1 text-gray-300 text-xs">(Ctrl+Enter)</span>
-          </button>
-          <button
-            onClick={handleClear}
-            className="bg-orange-600 hover:bg-orange-500 font-semibold text-white px-3 py-1 rounded text-xs flex items-center"
-          >
-            Limpar
-          </button>
-        </div>
       </div>
     </div>
   );
