dc7d1ca74c64fbf75b60a1791d9c61ef1c8f2ee7
Douglas Gusson <douglasgusson@gmail.com>
2025-05-07 19:59:54 -0300

    Adiciona suporte ao PWA com configuração de manifest no vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,47 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import tailwindcss from "@tailwindcss/vite";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    VitePWA({
+      registerType: "autoUpdate",
+      devOptions: {
+        enabled: true,
+      },
+      manifest: {
+        name: "Text tools",
+        short_name: "Text tools",
+        description: "Text tools",
+        theme_color: "#18181b",
+        background_color: "#ffffff",
+        display: "standalone",
+        lang: "en",
+        icons: [
+          {
+            src: "icons/icon-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "icons/icon-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+          {
+            src: "icons/icon-1024x1024.png",
+            sizes: "1024x1024",
+            type: "image/png",
+            purpose: "maskable",
+          },
+        ],
+      },
+    }),
+  ],
   worker: {
     format: "es",
   },
